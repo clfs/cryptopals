9151c882b648dc01c4facf9958250e53ada1d929
Calvin Figuereo-Supraner <mail@calvin.page>
2024-07-20 20:50:26 -0700

    Solve challenge 10

--- set2.go
+++ set2.go
@@ -2,13 +2,15 @@ package cryptopals
 
 import (
 	"bytes"
+	"crypto/cipher"
+	"crypto/subtle"
 	"math"
 )
 
 // pkcs7pad appends PKCS#7 padding to b to guarantee block size n. It returns
 // the updated slice.
 func pkcs7pad(b []byte, n int) []byte {
-	if n < 0 || n >= math.MaxUint8 {
+	if n < 0 || n > math.MaxUint8 {
 		panic("invalid block size")
 	}
 
@@ -17,4 +19,69 @@ func pkcs7pad(b []byte, n int) []byte {
 	padding := bytes.Repeat([]byte{p}, int(p))
 
 	return append(b, padding...)
+}
+
+type cbcDecrypter struct {
+	b  cipher.Block
+	iv []byte
+}
+
+func (c *cbcDecrypter) BlockSize() int {
+	return c.b.BlockSize()
+}
+
+func (c *cbcDecrypter) CryptBlocks(dst, src []byte) {
+	bs := c.b.BlockSize()
+
+	if len(src)%bs != 0 {
+		panic("input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("dst too small")
+	}
+	if len(src) == 0 {
+		return
+	}
+
+	// Loop over the blocks backwards to reduce copying.
+	//
+	// See crypto/cipher/cbc.go in the standard library for the technique
+	// this is inspired by.
+
+	var (
+		prev  = len(src) - 2*bs // Start of the previous block.
+		start = len(src) - bs   // Start of the current block.
+		end   = len(src)        // End of the current block.
+	)
+
+	// Save this to use as the new IV later.
+	tmp := bytes.Clone(src[start:end])
+
+	// Loop over every block but the first one.
+	for start > 0 {
+		// Decrypt the current ciphertext block into the current plaintext block.
+		c.b.Decrypt(dst[start:end], src[start:end])
+
+		// XOR the previous ciphertext block into the current plaintext block.
+		subtle.XORBytes(dst[start:end], dst[start:end], src[prev:start])
+
+		// Move backwards a block.
+		end -= bs
+		start -= bs
+		prev -= bs
+	}
+
+	// The first block uses the IV in place of a "previous ciphertext block".
+	c.b.Decrypt(dst[start:end], src[start:end])
+	subtle.XORBytes(dst[start:end], dst[start:end], c.iv)
+
+	// Update the IV in preparation for subsequent calls.
+	c.iv = tmp
+}
+
+func newCBCDecrypter(b cipher.Block, iv []byte) cipher.BlockMode {
+	if len(iv) != b.BlockSize() {
+		panic("invalid iv length")
+	}
+	return &cbcDecrypter{b, iv}
 }
--- set2_test.go
+++ set2_test.go
@@ -2,10 +2,11 @@ package cryptopals
 
 import (
 	"bytes"
+	"crypto/aes"
 	"testing"
 )
 
-func TestChallenge10(t *testing.T) {
+func TestChallenge9(t *testing.T) {
 	in := []byte("YELLOW SUBMARINE")
 	n := 20
 	want := []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
@@ -14,4 +15,27 @@ func TestChallenge10(t *testing.T) {
 	if !bytes.Equal(want, got) {
 		t.Errorf("want %q, got %q", want, got)
 	}
+}
+
+func TestChallenge10(t *testing.T) {
+	in := decodeBase64FromFile(t, "testdata/10.txt")
+	key := []byte("YELLOW SUBMARINE")
+	iv := make([]byte, 16)
+	want := []byte("I'm back and I'm ringin' the bell \nA rockin' on") // first few bytes
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mode := newCBCDecrypter(block, iv)
+
+	mode.CryptBlocks(in, in)
+
+	got := in[:len(want)]
+	if !bytes.Equal(want, got) {
+		t.Errorf("wrong first bytes: want %q, got %q", want, got)
+	}
+
+	t.Logf("plaintext: %q", in)
 }
